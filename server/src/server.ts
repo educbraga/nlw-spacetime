7e22fcfc715d73da0f0b476fde562904ab14ef96
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-23 19:41:50 -0300

    adiciona fastify

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,13 @@
-const nome: string = 'Eduardo'
-console.log(nome)
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('📦 HTTP server running on http://localhost:3333')
+})
