ec7fddad631d9ddfcbc32f13c4013c05e06d3042
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-29 00:10:56 -0300

    adiciona configuração básica do mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Lia 📦</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-5xl font-bold text-gray-50">Hello World 📦</Text>
+      <StatusBar style="light" translucent />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontWeight: '700',
-    fontSize: 100,
-  }
-});
